f1b137ad1214dded7918fb65a9a8e4dc9cb246d1
Ivan <Ivan@Mac-mini-IvanO.local>
2017-11-06 19:36:42 +0200

    video fix commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-  $('.main-wrapper').on('load scroll', function () {
+  $(window).on('load scroll', function () {
     var scrolled = $(this).scrollTop();
     $('#headerVideo').css('transform', 'translate3d(0, ' + (scrolled * 0.25) + 'px, 0)'); // parallax (25% scroll rate)
   });
@@ -9,15 +9,15 @@ $(document).ready(function () {
     scrollspeed: 50,
   });*/
 
-    $('.main-wrapper').on('load scroll', function () {
-        var scrolledUp = $(this).scrollTop();
-        var headerVideo = $('.header-video ').height();
-        console.log(scrolledUp, headerVideo);
-        if (scrolledUp >= headerVideo) {
-            $("#videoBottom").addClass('video-bottom_fixed');
-        } else {
-            $("#videoBottom").removeClass('video-bottom_fixed');
-        }
-    });
+  $(window).on('load scroll', function () {
+      var scrolledUp = $(this).scrollTop();
+      var headerVideo = $('.header-video ').height();
+      if (scrolledUp >= headerVideo) {
+          $("#videoBottom").addClass('video-bottom_fixed');
+      } else {
+          $("#videoBottom").removeClass('video-bottom_fixed');
+      }
+  });
 
+  $("video.header-video").get(0).play();
 });
